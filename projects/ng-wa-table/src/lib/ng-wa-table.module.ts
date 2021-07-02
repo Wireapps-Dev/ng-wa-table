c4cfb0a49b1a38d828f65ca4417d030fe9ef8a98
Hasitha Walpola <hasitha@redlabs.tech>
2021-07-02 17:07:07 +0530

    Package.json changes

--- projects/ng-wa-table/src/lib/ng-wa-table.module.ts
+++ projects/ng-wa-table/src/lib/ng-wa-table.module.ts
@@ -1,5 +1,5 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgWaTableComponent } from './ng-wa-table.component';
 
 
@@ -9,7 +9,7 @@ import { NgWaTableComponent } from './ng-wa-table.component';
     NgWaTableComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule
   ],
   exports: [
     NgWaTableComponent
